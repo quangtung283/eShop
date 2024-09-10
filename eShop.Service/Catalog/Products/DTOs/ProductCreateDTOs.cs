58f31a93dcfe68eb4b439364b4a5e1d922017381
Dinh Quang Tung <quangtung2832002fpt@gmail.com>
2024-09-10 10:47:18 +0700

    add project Service ,DTOs,IRepository

---
+++ eShop.Service/Catalog/Products/DTOs/ProductCreateDTOs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.Service.Catalog.Products.DTOs
+{
+    public class ProductCreateDTOs
+    {
+        public int Id { set; get; }
+        public decimal Price { set; get; }
+        public decimal OriginalPrice { set; get; }
+        public int Stock { set; get; }
+        public int ViewCount { set; get; }
+        public DateTime DateCreated { set; get; }
+
+        public bool? IsFeatured { get; set; }
+
+    }
+}
---
+++ eShop.Service/Catalog/Products/IManageProductService.cs
@@ -0,0 +1,20 @@
+﻿using eShop.Service.Catalog.Products.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.Service.Catalog.Products
+{
+    public interface IManageProductService
+    {
+        int Create(ProductCreateDTOs createProduct);
+
+        int Update(UpdateProductDTOs updateProduct);
+
+        int Delete(int productId); 
+        List<ProductViewModel> GetAll();
+        List<ProductViewModel> GetAllPaging(string keyword , int pageSize , int pageIndex);
+    }
+}
